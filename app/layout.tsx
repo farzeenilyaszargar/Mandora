a160aafba1752322dc9511bae0c5399e0a73f0fd
farzeenilyaszargar <farzeenilyaszargar@gmail.com>
2026-09-16 17:46:18 +0530

    Prevent mobile page zoom

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Fustat, Ma_Shan_Zheng } from "next/font/google";
 import "./globals.css";
 
@@ -58,6 +58,13 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
